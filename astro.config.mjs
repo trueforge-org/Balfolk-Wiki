53f7ab6f2bf3ca26160e22d697c97a26120c39a0
Kjeld Schouten <info@kjeldschouten.nl>
2025-12-17 01:19:22 +0100

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -77,8 +77,6 @@ export default defineConfig({
       components: {
         Header: "./src/components/CustomHeader.astro",
         Sidebar: "./src/components/CustomSidebar.astro",
-        // Hero: "./src/components/CustomHero.astro",
-        // MarkdownContent: "./src/components/CustomMarkdownContent.astro",
       },
       plugins: [
         // starlightBlog({
